67ea401efab24d405dd745d1b82100c183919e75
savannahvaith <sav.vaithilingam@gmail.com>
2020-11-27 12:04:50 +0000

    added ticking clock example

---
+++ cne_demos/src/Components/04-State/Clock.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'; 
+import { useEffect } from 'react';
+
+const Clock = () => {
+
+    let [date,setDate] = useState(new Date().toLocaleTimeString());
+
+    useEffect( () => {
+        setInterval(
+            tick, 1000
+        )
+    })
+
+    function tick(){
+        setDate(date = new Date().toLocaleTimeString());
+    }
+
+    
+    return(
+        <>
+        <h1> What time is it?</h1>
+        <p>It is {date}</p>
+        </>
+    )
+}
+
+export default Clock; 
